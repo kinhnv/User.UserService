4a8894248703f260555cbe40d489df25d7e53e25
Nguyen Van Kinh <kinhnv1801@gmail.com>
2022-10-16 19:24:23 +0700

    change config of user service and identity server

--- TestMaker.UserService.Infrastructure/InfrastructureRegister.cs
+++ TestMaker.UserService.Infrastructure/InfrastructureRegister.cs
@@ -1,4 +1,5 @@
-﻿using Ddd.Helpers;
+﻿using AspNetCore.Environment.Extensions;
+using Ddd.Helpers;
 using IdentityServer4.Services;
 using IdentityServer4.Validation;
 using Microsoft.AspNetCore.Builder;
@@ -29,10 +30,10 @@ namespace TestMaker.UserService.Infrastructure
         {
             service.AddDbContext<ApplicationDbContext>(optionsBuilder =>
             {
-                optionsBuilder.UseSqlServer(configuration["Mssql:ConnectionString"]);
+                optionsBuilder.UseSqlServer(configuration.GetConfiguration("Mssql:ConnectionString"));
             });
 
-            service.AddMongoContext(configuration["Mongodb:ConnectionString"]);
+            service.AddMongoContext(configuration.GetConfiguration("Mongodb:ConnectionString"));
 
             service.AddAutoMapperProfiles();
 
