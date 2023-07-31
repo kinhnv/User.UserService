2d027733ef0930a18c077c53a3d6f9cfd2bfc039
Nguyen Van Kinh <kinhnv1801@gmail.com>
2023-07-31 19:07:44 +0700

    update

--- i3rothers.UserServiceClient/projects/user-service-client/src/public-api.ts
+++ i3rothers.UserServiceClient/projects/user-service-client/src/public-api.ts
@@ -2,6 +2,4 @@
  * Public API Surface of user-service-client
  */
 
-export * from './lib/user-service-client.service';
-export * from './lib/user-service-client.component';
-export * from './lib/user-service-client.module';
+export * from './lib/user-service-client';
